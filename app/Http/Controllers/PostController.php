676a2255b19594a8251e7b836f089a8db3a31d35
MomotaroCat <taiyo.Kayahara@gmail.com>
2023-09-01 23:52:52 +0900

    リレーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Requests\PostRequest;
-use App\Models\Post;
+use App\Models\Category;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     } 
     
     public function show(Post $post)
@@ -18,12 +19,12 @@ class PostController extends Controller
         return view('posts.show')->with(['post' => $post]);
     }
     
-    public function create()
+    public function create(Category $category)
     {
-        return view('posts.create');
+        return view('posts.create')->with(['categories' => $category->get()]);
     }
     
-    public function store(PostRequest $request, Post $post)
+    public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
         $post->fill($input)->save();
